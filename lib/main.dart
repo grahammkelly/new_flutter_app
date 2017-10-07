3014b997dda745c8c0bbd1f4d4c740c7b8238e39
Graham Kelly <graham.kelly@mttnow.com>
2017-10-08 00:32:11 +0100

    Display the chat list

--- lib/main.dart
+++ lib/main.dart
@@ -66,10 +66,25 @@ class ChatScreenState extends State<ChatScreen> {
     @override
     Widget build(BuildContext context) {
       return new Scaffold(
-        appBar: new AppBar(
-            title: new Text("FriendlyChat")
+        appBar: new AppBar(title: new Text("FriendlyChat")),
+        body: new Column(
+            children: <Widget>[
+              new Flexible(
+                  child: new ListView.builder(
+                    padding: new EdgeInsets.all(8.0),
+                    reverse: true,
+                    itemBuilder: (_, int index) => _messages[index],
+                    itemCount: _messages.length,
+                  )
+              ),
+              new Divider(height: 1.0),
+              new Container(
+                decoration: new BoxDecoration(
+                    color: Theme.of(context).cardColor),
+                child: _buildTextComposer(),
+              ),
+            ]
         ),
-        body: _buildTextComposer(),
       );
     }
 }
