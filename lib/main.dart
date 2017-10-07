3dc5c28adea3cdb7942bfc7319b67dc938101e7f
Graham Kelly <graham.kelly@mttnow.com>
2017-10-08 00:13:06 +0100

    Added button. Now 'sends' message (actually, just clears it)

--- lib/main.dart
+++ lib/main.dart
@@ -22,22 +22,27 @@ class ChatScreen extends StatefulWidget {
 class ChatScreenState extends State<ChatScreen> {
   final TextEditingController _textController = new TextEditingController();
 
-  Widget _buildTextComposer() {
-    return new Container(
+  Widget _buildTextComposer() => new Container(
       margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: new Row(                                              //new
-        children: <Widget>[                                      //new
+      child: new Row(
+        children: <Widget>[
           new Flexible(
             child: new TextField(
               controller: _textController,
               onSubmitted: _handleSubmitted,
               decoration: new InputDecoration.collapsed(hintText: "Send a message"),
             ),
+          ),
+          new Container(
+            margin: new EdgeInsets.symmetric(horizontal: 4.0),
+            child: new IconButton(
+                icon: new Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text)
+            ),
           )
         ]
-      )
+      ),
     );
-  }
 
   void _handleSubmitted(String text) {
     _textController.clear();
