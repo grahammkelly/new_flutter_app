0f8329fb8fb86a5d16da3047ed2c21e2c83f40b8
Graham Kelly <graham.kelly@mttnow.com>
2017-10-08 00:18:47 +0100

    Changed colour of the icon to the right

--- lib/main.dart
+++ lib/main.dart
@@ -22,39 +22,46 @@ class ChatScreen extends StatefulWidget {
 class ChatScreenState extends State<ChatScreen> {
   final TextEditingController _textController = new TextEditingController();
 
-  Widget _buildTextComposer() => new Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: new Row(
-        children: <Widget>[
-          new Flexible(
-            child: new TextField(
-              controller: _textController,
-              onSubmitted: _handleSubmitted,
-              decoration: new InputDecoration.collapsed(hintText: "Send a message"),
-            ),
-          ),
-          new Container(
-            margin: new EdgeInsets.symmetric(horizontal: 4.0),
-            child: new IconButton(
-                icon: new Icon(Icons.send),
-                onPressed: () => _handleSubmitted(_textController.text)
-            ),
-          )
-        ]
+  Widget _buildTextComposer() {
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+            children: <Widget>[
+              new Flexible(
+                child: new TextField(
+                  controller: _textController,
+                  onSubmitted: _handleSubmitted,
+                  decoration: new InputDecoration.collapsed(
+                      hintText: "Send a message"),
+                ),
+              ),
+              new Container(
+                margin: new EdgeInsets.symmetric(horizontal: 4.0),
+                child: new IconButton(
+                    icon: new Icon(Icons.send),
+                    onPressed: () => _handleSubmitted(_textController.text)
+                ),
+              )
+            ]
+        ),
       ),
     );
-
-  void _handleSubmitted(String text) {
-    _textController.clear();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-          title: new Text("FriendlyChat")
-      ),
-      body: _buildTextComposer(),
-    );
-  }
+    void _handleSubmitted(String text) {
+      _textController.clear();
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return new Scaffold(
+        appBar: new AppBar(
+            title: new Text("FriendlyChat")
+        ),
+        body: _buildTextComposer(),
+      );
+    }
+
 }
